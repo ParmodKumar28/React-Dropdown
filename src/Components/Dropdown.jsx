bd551f28371b6a59de67f733bd70c466ccb41998
Parmod Kumar <yparmodyadav@gmail.com>
2024-01-21 17:29:17 +0530

    Ui

--- src/Components/Dropdown.jsx
+++ src/Components/Dropdown.jsx
@@ -3,30 +3,40 @@ import styles from "./Dropdown.module.css";
 import items from "./data";
 
 // Dropdown component
-export default function Dropdown(){
+export default function Dropdown() {
     // States
     const [listItems, setListItems] = useState(items);
-    const [dropdownVisible, setDropdownVisible] = useState(false);
+    const [dropdownVisible, setDropdownVisible] = useState(true);
+    const [itemSelected, setItemSelected] = useState("");
 
     // Handler Functions
 
+
     // Returning JSX
-    return(
+    return (
         <div className={styles.dropdownContainer}>
-            <h1 className={styles.heading}>Should you use a dropdown</h1>
+            <h1 className={styles.heading}>Should you use a dropdown ?</h1>
+            <div className={styles.dropdownWrapper}>
             {/* Dropdown */}
             <div className={styles.dropdownToggle}>
-                <p>Select</p>
-                <img src="https://cdn-icons-png.flaticon.com/128/10728/10728680.png" alt="Icon"/>
+                <p className={styles.toggleText}>Select</p>
+                <img className={styles.icon} src={dropdownVisible ?
+                    "  https://cdn-icons-png.flaticon.com/128/271/271228.png"
+                    : "https://cdn-icons-png.flaticon.com/128/10728/10728680.png"}
+                     alt="Icon" 
+                />
             </div>
             {/* Dropdown list items */}
-            <ul className={styles.listItems}>
-                {listItems.map((item, i) => (
-                    <li key={i} className={styles.item}>
-                        {item}
-                    </li>
-                ))}
-            </ul>
+            {dropdownVisible ?
+                <div className={styles.listItems}>
+                    {listItems.map((item, i) => (
+                        <div key={i} className={styles.item}>
+                            {item}
+                        </div>
+                    ))}
+                </div> : null
+            }
+             </div>
         </div>
     )
 }
